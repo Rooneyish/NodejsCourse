cb2bc221086ff9330e02aaa911a5d8674b3d4825
rooneyish <ilustraterp@gmail.com>
2025-03-19 08:54:18 +0545

    User creation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,28 @@
-export const registerUser = () =>{
-    console.log("registerUser");
-}
+import User from "../models/user.js";
 
-export const loginUser = () =>{
-    console.log("loginUser");
-}
+export const registerUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    res.json({
+      success: true,
+      message: "User registered successfully !",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const loginUser = () => {
+  console.log("testing");
+};
--- database/mongodb.js
+++ database/mongodb.js
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+
+const connectDB = async() =>{
+    try{
+        await mongoose.connect("mongodb+srv://firstUser:User543@cluster0.rzvox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        console.log("Connected to the database successfully.")
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+export default connectDB;
